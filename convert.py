c29fde4df4ba2cfdef1928bd9149a75655215684
Manucho Adero <42127278+manucho254@users.noreply.github.com>
2020-12-27 20:53:26 +0300

    Better and cleaner look

--- convert.py
+++ convert.py
@@ -3,37 +3,25 @@ from gtts import gTTS
 from tkinter import *
 import speech_recognition as sr
 from tkinter import messagebox
+import time 
 
 
 root = Tk()
 
 root.title("Convert text to speech")
 
-frame1 = Frame(root, 
-               bg = "grey",  
-               height = "150")
-root.geometry('400x400')
-root.resizable(width=100, height=100)
-frame1.pack(fill = X) 
-  
-  
-frame2 = Frame(root,  
-               bg = "grey",  
-               height = "750")
-frame2.pack(fill=X) 
-
-label = Label(frame1, text = "Text And Speach Converter",  
+root.geometry('530x300')
+root.resizable(width=0, height=0)
+   
+label = Label(root, text = "Text And Speach Converter",  
               font = "bold, 30", 
-              bg = "cyan" , padx=6, pady=13,
-              ) 
-  
-label.place(x = 100, y = 60) 
+              bg = "green", fg="white" , padx=6, pady=13,
+              )
+label.grid(row=0,columnspan=3, padx=10, pady=10)
 
-entry = Entry(frame2, width = 45,  
+entry = Entry(root, width = 45,  
               bd = 6, font = 14, borderwidth=6) 
-entry.grid(row=0, column=0, columnspan=3, padx=10, pady=13)
-entry.place(x = 130, y = 52) 
-entry.insert(0, "")
+entry.grid(row=1, column=1, columnspan=1, padx=10, pady=13)
 
 # text to audio conversion function
 def text_to_audio():
@@ -53,14 +41,9 @@ def text_to_audio():
     os.system('start simp.mp3')
 
 
-button_text = Button(frame2, text="Text Convert" ,
-    padx=30 , pady=10 , 
-    bg = 'green',command=text_to_audio 
-    )
 w = Message(root, text="this is a message")
-w.pack()
-button_text.place(x = 100,  
-          y = 130)
+
+
 # sound to text conversion function
 def Sound_to_text():
     r = sr.Recognizer()
@@ -75,15 +58,6 @@ def Sound_to_text():
         except:
             messagebox.showerror("error", "try again")
 
-button_sound = Button(
-     frame2, text="SPEAK" ,
-    padx=30 , pady=10 , 
-    bg = 'blue',command= Sound_to_text
-    )
-
-button_sound.place(x = 300,  
-          y = 130)
-
 # exit the progarm function
 def button_exit():
     MsgBox = messagebox.askquestion ('Exit Application','Are you sure you want to exit the application',icon = 'warning')
@@ -93,13 +67,30 @@ def button_exit():
         messagebox.showinfo('Return','You will now return to the application screen')
     return
 
+button_text = Button(root, text="CONVERT TEXT" ,
+    padx=30 , pady=10 , 
+    bg = 'green',fg="white",command=text_to_audio 
+    )
+w = Message(root, text="this is a message")
+button_text.place(x = 100,  
+          y = 180)
+
+button_sound = Button(
+     root, text="SPEAK" ,
+    padx=30 , pady=10 , 
+    bg = 'green', fg="white",command= Sound_to_text
+    )
+
+button_sound.place(x = 300,  
+          y = 180)
+
 button_exit = Button(
-    frame2, text="Exit" ,
+    root, text="Exit" ,
     padx=30, pady=10, 
     bg = 'red', command=button_exit
     )
 
-button_exit.place(x = 500,
-          y = 130)
+button_exit.place(x = 220,
+          y = 250)
 
 root.mainloop()
